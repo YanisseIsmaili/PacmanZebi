d43facb72ef4dea03d92d25da5635d491258ed62
Lukas Taboga <lukastaboga@Lukass-MacBook-Pro.local>
2023-09-29 10:43:07 +0200

    test

--- MazePointPosition.go
+++
@@ -1,6 +0,0 @@
-func getMazePointFromPosition(x float64, y float64) (int, int) {
-
-    col := int(math.Round(x/float64(blockSize)))
-    row := int(math.Round(y/float64(blockSize)))
-    return col, row
-}
--- PositionMazePoint.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func getPositionFromMazePoint(col int, row int) (float64, float64) {
-    // here we are casting to float as we standard position usage in ebiten is float
-    return float64(blockSize*col), float64(blockSize*row)
-}
